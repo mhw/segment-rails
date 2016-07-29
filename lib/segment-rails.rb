49c07f7f316ccb8ddbe46cd78cdd2f8730120b6f
Mark H. Wilkinson <mhw@dangerous-techniques.com>
2016-07-29 16:30:53 +0100

    Remove the Rails module completely as it's unnecessary.

--- lib/segment-rails.rb
+++ lib/segment-rails.rb
@@ -3,9 +3,7 @@ require 'segment-rails/helpers'
 
 module SegmentRails
   if const_defined? :Rails
-    module Rails
-      class Engine < ::Rails::Engine
-      end
+    class Engine < ::Rails::Engine
     end
   end
 end
